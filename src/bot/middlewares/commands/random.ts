db947483f22595319deb3fd8dd5d05501b008781
Revincx <revincx@outlook.com>
2022-01-17 19:49:17 +0800

    Fix random cammand err catch

--- src/bot/middlewares/commands/random.ts
+++ src/bot/middlewares/commands/random.ts
@@ -4,28 +4,44 @@ import { getArtwork } from "~/database/operations/artwork"
 import { ChannelMessage } from "~/types/Message"
 
 export default Telegraf.command('random', async ctx => {
-    let waiting_message = await ctx.reply('正在随机获取一张壁纸...', {
-        reply_to_message_id: ctx.message.message_id
-    })
-    let messages = await MessageModel.aggregate([
-        {
-            $match: { type: 'photo' }
-        },
-        {
-            $sample: { size: 1 }
-        }
-    ])
-    if (messages.length == 0) {
-        return await ctx.telegram.editMessageText(waiting_message.chat.id, waiting_message.message_id, undefined, "诶呀，获取图片失败了~")
-    }
-    let message = messages[0] as ChannelMessage
-    let artwork = await getArtwork(message.artwork_index)
-    await ctx.replyWithPhoto(message.file_id, {
-        reply_to_message_id: ctx.message.message_id,
-        caption: '这是你要的壁纸~',
-        ...Markup.inlineKeyboard([
-            Markup.button.url('作品来源', artwork.source.post_url)
+    // let waiting_message = await ctx.reply('正在随机获取一张壁纸...', {
+    //     reply_to_message_id: ctx.message.message_id
+    // })
+    try {
+        let messages = await MessageModel.aggregate([
+            {
+                $match: { type: 'photo' }
+            },
+            {
+                $sample: { size: 1 }
+            }
         ])
-    })
-    return await ctx.deleteMessage(waiting_message.message_id)
+        if (messages.length == 0) {
+            return await ctx.reply("诶呀，获取图片失败了~", {
+                reply_to_message_id: ctx.message.message_id
+            })
+        }
+        let message = messages[0] as ChannelMessage
+        let artwork = await getArtwork(message.artwork_index)
+        return await ctx.replyWithPhoto(message.file_id, {
+            reply_to_message_id: ctx.message.message_id,
+            caption: '这是你要的壁纸~',
+            ...Markup.inlineKeyboard([
+                Markup.button.url('作品来源', artwork.source.post_url)
+            ])
+        })
+    }
+    catch (err) {
+        console.log(err)
+        if (err instanceof Error) {
+            return await ctx.reply('操作失败: ' + err.message, {
+                reply_to_message_id: ctx.message.message_id
+            })
+        }
+        return await ctx.reply('操作失败: 未知错误', {
+            reply_to_message_id: ctx.message.message_id
+        })
+    }
+
+    // return await ctx.deleteMessage(waiting_message.message_id)
 })
