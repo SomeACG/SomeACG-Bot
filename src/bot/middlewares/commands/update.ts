05a5024d7e243d271c5c8e9236c02a114186ad16
Revincx <revincx@outlook.com>
2022-01-25 22:01:18 +0800

    Fix update command reply mode

--- src/bot/middlewares/commands/update.ts
+++ src/bot/middlewares/commands/update.ts
@@ -1,6 +1,7 @@
 import { Telegraf } from "telegraf";
 import { Message } from "telegraf/typings/core/types/typegram";
 import { getArtwork } from "~/database/operations/artwork";
+import { getMessage } from "~/database/operations/message";
 import { modifyArtwork } from "~/services/artwork-service";
 import { parseParams } from "~/utils/param-parser";
 
@@ -12,7 +13,7 @@ export default Telegraf.command('update', async ctx => {
             await ctx.deleteMessage(waiting_message.message_id) 
         }
     }, 10000)
-    if (!command.params['index']) return await ctx.reply('请回复一条消息或指定要更改的作品序号!', {
+    if (!command.params['index'] && !ctx.message.reply_to_message) return await ctx.reply('请回复一条消息或指定要更改的作品序号!', {
         reply_to_message_id: ctx.message.message_id
     })
     waiting_message = await ctx.reply('正在更新作品信息...', {
@@ -20,7 +21,17 @@ export default Telegraf.command('update', async ctx => {
     })
     try {
 
-        let artwork_index = parseInt(command.params['index'])
+        let artwork_index = -1
+        if(command.params['index'])artwork_index = parseInt(command.params['index'])
+        if(ctx.message.reply_to_message)
+        {
+            let reply_to_message = ctx.message.reply_to_message as Message.CommonMessage
+            if (!reply_to_message.forward_from_message_id) return await ctx.reply('回复的消息不是有效的频道消息！', {
+                reply_to_message_id: ctx.message.message_id
+            })
+            let message = await getMessage(reply_to_message.forward_from_message_id)
+            artwork_index = message.artwork_index
+        }
         let artwork = await getArtwork(artwork_index)
         let param_keys = Object.keys(command.params)
         for (let key of param_keys) {
