d45f3b41130e2bcbf3c123e4320c2bfc08c33e88
Revincx <revincx@outlook.com>
2022-07-01 13:57:14 +0800

    Add empty hint when no results returned

--- src/bot/middlewares/inline/index.ts
+++ src/bot/middlewares/inline/index.ts
@@ -12,16 +12,30 @@ export default Telegraf.on('inline_query', async ctx => {
         artwork_results = await getArtworksByTags(tags)
     }
 
-    let results = artwork_results.map((artwork, index) => ({
-        type: "photo",
-        id: ctx.inlineQuery.id + '-' + index,
-        photo_file_id: artwork.photo_file_id,
-        caption: "这是你要的壁纸~",
-        reply_markup: Markup.inlineKeyboard([
-            Markup.button.url('作品来源', artwork.source.post_url),
-            Markup.button.url('获取原图', `https://t.me/${ctx.me}?start=document-${artwork.index}`)
-        ]).reply_markup
-    }))
+    let results = []
+
+    if (artwork_results?.length == 0) {
+        results = [{
+            type: "article",
+            id: ctx.inlineQuery.id,
+            title: "没有找到结果呢，试着换个关键词吧~",
+            input_message_content: {
+                message_text: "没有找到结果呢，试着换个关键词吧~"
+            }
+        }]
+    }
+    else {
+        results = artwork_results.map((artwork, index) => ({
+            type: "photo",
+            id: ctx.inlineQuery.id + '-' + index,
+            photo_file_id: artwork.photo_file_id,
+            caption: "这是你要的壁纸~",
+            reply_markup: Markup.inlineKeyboard([
+                Markup.button.url('作品来源', artwork.source.post_url),
+                Markup.button.url('获取原图', `https://t.me/${ctx.me}?start=document-${artwork.index}`)
+            ]).reply_markup
+        }))
+    }
 
     // @ts-ignore
     return await ctx.answerInlineQuery(results);
