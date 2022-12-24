df5434ce005d581521d9dbca78f6c79e672412be
Revincx <revincx233@gmail.com>
2022-12-24 19:28:10 +0800

    fix: pixiv urls is null

--- src/platforms/pixiv.ts
+++ src/platforms/pixiv.ts
@@ -1,6 +1,7 @@
 import axios from '~/utils/axios';
 import path from 'path';
 import { ArtworkInfo, ImageSize } from '~/types/Artwork';
+import logger from '~/utils/logger';
 
 export default async function getArtworkInfo(
     post_url: string,
@@ -14,22 +15,21 @@ export default async function getArtworkInfo(
     const pixivData = responseData['body'];
     if (picture_index > pixivData['pageCount'] - 1)
         throw new Error('Picture index out of range');
-    let urls = pixivData['urls'];
-    let size: ImageSize = {
-        width: pixivData['width'],
-        height: pixivData['height']
+    // let urls = pixivData['urls'];
+    // let size: ImageSize = {
+    //     width: pixivData['width'],
+    //     height: pixivData['height']
+    // };
+
+    const { data } = await axios.get(
+        `https://www.pixiv.net/ajax/illust/${pixiv_id}/pages?lang=zh`
+    );
+    const pageData = data['body'];
+    const urls = pageData[picture_index]['urls'];
+    const size = {
+        width: pageData[picture_index]['width'],
+        height: pageData[picture_index]['height']
     };
-    if (picture_index != 0) {
-        const { data } = await axios.get(
-            `https://www.pixiv.net/ajax/illust/${pixiv_id}/pages?lang=zh`
-        );
-        const pageData = data['body'];
-        urls = pageData[picture_index]['urls'];
-        size = {
-            width: pageData[picture_index]['width'],
-            height: pageData[picture_index]['height']
-        };
-    }
 
     // 处理一下
     let desc = pixivData['description'] as string;
